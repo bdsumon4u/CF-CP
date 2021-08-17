88cf2cf7719402ef922e1015266210be746ed114
Sumon Ahmed <bdsumon4u@gmail.com>
2021-08-17 19:17:28 +0600

    50/A. Domino piling.cpp

---
+++ 50/A. Domino piling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int a = 2, b = 1;
+
+int main() {
+    int M, N;
+    cin >> M >> N;
+
+    cout << (M * N) / (a * b) << endl;
+
+    return 0;
+}
