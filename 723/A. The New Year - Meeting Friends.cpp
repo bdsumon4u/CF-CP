ff552314cd13803c0b90d6060976afd3fbf9b91b
Sumon Ahmed <bdsumon4u@gmail.com>
2021-08-22 23:48:13 +0600

    723/A. The New Year - Meeting Friends.cpp

---
+++ 723/A. The New Year - Meeting Friends.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+
+    cout << (abs(x1 - x2) + abs(x2 - x3) + abs(x3 - x1)) / 2 << endl;
+
+    return 0;
+}
